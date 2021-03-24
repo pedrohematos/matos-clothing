cc054deb7c254e5b2b25ec2902d8e4841ba2821c
unknown <pedrohematos@outlook.com>
2021-03-24 19:28:24 -0300

    reverse last server.js commit due to heroku error

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(compression);
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log("Server is running on port " + port);
+  console.log("Server running on port " + port);
 });
 
 app.get("/service-worker.js", (req, res) => {
